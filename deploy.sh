f5fb7fc6fb38c12d732ccf2095b946f249ea4236
DavidFalces <71983111+DavidFalces@users.noreply.github.com>
2024-03-01 14:55:28 +0100

    First version script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+GH_USER="DevOps-GroupF"
+GH_PASS=$(cat /deployment/secrets/github)
+REGISTRY="ghcr.io"
+
+echo $GH_PASS | sudo docker login $REGISTRY -u $GH_USER --password-stdin 
+docker compose pull
+docker compose up
+
+exit 0
