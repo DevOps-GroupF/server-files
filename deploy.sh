793bf7c4621473bda477b4c54998264d1670f0eb
P. Rouvillain <17237293+prouv@users.noreply.github.com>
2024-03-14 14:07:02 +0100

    add print statements

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,21 @@
 #! /bin/bash
+echo "Setting env variables"
 GH_USER=$(cat /deployment/secrets/github_pck_user)
 GH_PASS=$(cat /deployment/secrets/github_pck_token)
 export DB_CONN_STRING_LOC="/deployment/secrets/db_conn_string"
 REGISTRY="ghcr.io"
 
+echo "Logging in to dockerhub"
 echo $GH_PASS | docker login $REGISTRY -u $GH_USER --password-stdin
+
+echo "Pulling the image"
 docker compose pull
+
+echo "Starting the container"
 docker compose up -d
+
+echo "Logging out of dockerhub"
 docker logout $REGISTRY
 
+echo "Deploy script done"
 exit 0
